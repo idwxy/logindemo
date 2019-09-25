91672f61196a79889e39bb7ed7fcdaed55f5e004
idwxy <idwxy@live.com>
2019-09-25 18:55:47 +0800

    封装 redis 常用接口

---
+++ src/main/java/com/idwxy/logindemo/util/RedisUtil.java
@@ -0,0 +1,152 @@
+package com.idwxy.logindemo.util;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisUtil {
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 设置过期时间
+     * @param key
+     * @param time
+     * @return
+     */
+    public boolean expire(String key, long time) {
+        try {
+            if (time > 0) {
+                redisTemplate.expire(key, time, TimeUnit.SECONDS);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 获取 key 的过期时间
+     * @param key
+     * @return
+     */
+    public long getExpire(String key) {
+        return redisTemplate.getExpire(key, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 检测 key 是否存在
+     * @param key
+     * @return
+     */
+    public boolean hasKey(String key) {
+        try {
+            return redisTemplate.hasKey(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 删除 key
+     * @param key
+     */
+    @SuppressWarnings("unchecked")
+    public void del(String... key) {
+        if (key != null && key.length > 0) {
+            if (key.length == 1) {
+                redisTemplate.delete(key[0]);
+            } else {
+                redisTemplate.delete(CollectionUtils.arrayToList(key));
+            }
+        }
+    }
+
+    /**
+     * 获取 key
+     * @param key
+     * @return
+     */
+    public Object get(String key) {
+        return key == null ? null : redisTemplate.opsForValue().get(key);
+    }
+
+    /**
+     * 设置 key
+     * @param key
+     * @param value
+     * @return
+     */
+    public boolean set(String key, Object value) {
+        try {
+            redisTemplate.opsForValue().set(key, value);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 设置带有过期时间的 key，默认永不过期
+     * @param key
+     * @param value
+     * @param time
+     * @return
+     */
+    public boolean set(String key, Object value, long time) {
+        try {
+            if (time > 0) {
+                redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+            } else {
+                set(key, value);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * value + delta
+     * @param key
+     * @param delta
+     * @return
+     */
+    public long incr(String key, long delta) {
+        if (delta < 0 ) {
+            throw new RuntimeException("delta must > 0");
+        }
+        return redisTemplate.opsForValue().increment(key, delta);
+    }
+
+    /**
+     * value - delta
+     * @param key
+     * @param delta
+     * @return
+     */
+    public long decr(String key, long delta) {
+        if (delta < 0) {
+            throw new RuntimeException("delta must > 0");
+        }
+        return redisTemplate.opsForValue().increment(key, -delta);
+    }
+
+
+
+
+
+
+
+
+
+}
