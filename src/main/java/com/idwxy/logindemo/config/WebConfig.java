eb7d19bbe28ecad448c86c259a0dfa507a22f691
idwxy <idwxy@live.com>
2019-09-26 18:17:17 +0800

    配置拦截器

---
+++ src/main/java/com/idwxy/logindemo/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.idwxy.logindemo.config;
+
+import com.idwxy.logindemo.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/user/**").excludePathPatterns("/user/login");
+        super.addInterceptors(registry);
+    }
+}
